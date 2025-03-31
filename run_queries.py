11d1f144baeb65080011db104b4ede5d71fcc1eb
Riyaz Shaik <riyazshaik.1104@gmail.com>
2025-03-31 14:20:40 -0400

    Updated run_queries.py
    can be used to manually run the sql queries or all at a time

--- run_queries.py
+++ run_queries.py
@@ -6,7 +6,6 @@ def run_all_sql_files(directory, db_path="sports_bi.db"):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     
-    # List all .sql files in the given directory in sorted order
     sql_files = sorted([f for f in os.listdir(directory) if f.endswith(".sql")])
     
     for filename in sql_files:
@@ -14,12 +13,10 @@ def run_all_sql_files(directory, db_path="sports_bi.db"):
         print(f"\n--- Running queries from file: {filename} ---")
         with open(file_path, "r") as file:
             sql = file.read()
-        
-        # Split the file content by semicolon to separate individual queries
         queries = sql.strip().split(";")
         
         for query in queries:
-            if query.strip():  # Skip empty queries
+            if query.strip():
                 print(f"\nRunning Query:\n{query.strip()}")
                 try:
                     cursor.execute(query.strip())
